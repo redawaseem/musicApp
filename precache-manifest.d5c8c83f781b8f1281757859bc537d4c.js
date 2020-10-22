331a2d32557695297c6837e08d334597289dd84d
redawaseem <65788008+redawaseem@users.noreply.github.com>
2020-10-22 07:25:46 -0400

    Updates

---
+++ precache-manifest.d5c8c83f781b8f1281757859bc537d4c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a249f6eb6a67092bec3d13233e6d11b",
+    "url": "/musicApp/index.html"
+  },
+  {
+    "revision": "122774b50de7d3a4ab95",
+    "url": "/musicApp/static/css/main.08035060.chunk.css"
+  },
+  {
+    "revision": "788459d18c43c735c57f",
+    "url": "/musicApp/static/js/2.2d245571.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/musicApp/static/js/2.2d245571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "122774b50de7d3a4ab95",
+    "url": "/musicApp/static/js/main.950b83f4.chunk.js"
+  },
+  {
+    "revision": "c225cdaa28030162754b",
+    "url": "/musicApp/static/js/runtime-main.0cfba719.js"
+  },
+  {
+    "revision": "f8ca295142a8f24b35e9be4aaeb07a0c",
+    "url": "/musicApp/static/media/charcoalBackground.f8ca2951.jpg"
+  },
+  {
+    "revision": "c3dc5e218e3b59d37c6d44b6e3b2af77",
+    "url": "/musicApp/static/media/iphoneVectorMain.c3dc5e21.png"
+  }
+]);
